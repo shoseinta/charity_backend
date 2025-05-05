b133f3e06bc735d2928e340c14c486d2302dd7f0
shoseinta <htaheri.76@gmail.com>
2025-05-05 15:59:53 +0330

    celery config

--- charity_backend/settings.py
+++ charity_backend/settings.py
@@ -195,3 +195,10 @@ SILKY_INTERCEPT_PERCENT = 100  # Profile every request
 
 
 
+# Celery Configuration
+CELERY_BROKER_URL = 'redis://localhost:6379/0'  # Redis as broker
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'  # Store results in Redis
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'UTC'
